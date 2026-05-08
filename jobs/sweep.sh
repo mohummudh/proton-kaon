c5d2d512e0a10af096263a3b8a479aa2a4ed21bd
Mohammed Sultan <mokhalidsultan@gmail.com>
2026-05-08 23:57:58 +0100

    csfffff

--- jobs/sweep.sh
+++ jobs/sweep.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=pk_sweep
-#SBATCH --partition=gpuA40GB
+#SBATCH --partition=gpuL
+#SBATCH --account=gpu-free40GB
 #SBATCH --gres=gpu:1
 #SBATCH --time=96:00:00
 #SBATCH --mem=32G
